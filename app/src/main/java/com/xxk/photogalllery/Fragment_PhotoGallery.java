b405f02488fbb4dff61ce9141de62b5290f12866
jianxuewei <jianxuewei@126.com>
2015-12-11 22:11:40 +0800

    download thumbnail image success.

--- app/src/main/java/com/xxk/photogalllery/Fragment_PhotoGallery.java
+++ app/src/main/java/com/xxk/photogalllery/Fragment_PhotoGallery.java
@@ -1,5 +1,6 @@
 package com.xxk.photogalllery;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -10,9 +11,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,8 +30,8 @@ import java.util.ArrayList;
  */
 public class Fragment_PhotoGallery extends Fragment {
     private GridView mGridView;
-    private TextView mTextView;
-    private ImageView mImageView;
+    //private TextView mTextView;
+    //private ImageView mImageView;
     private ArrayList<Image> mImages = new ArrayList<>();
 
     @Override
@@ -41,12 +45,12 @@ public class Fragment_PhotoGallery extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View v=inflater.inflate(R.layout.fragment_photo_gallery,container,false);
-//        mGridView= (GridView) v.findViewById(R.id.gridview);
-        View v = inflater.inflate(R.layout.temp, container, false);
-        mTextView = (TextView) v.findViewById(R.id.tv);
-        mImageView= (ImageView) v.findViewById(R.id.imageview);
-        new BaiduConnectAsyncTask().execute("http://image.baidu.com/data/imgs?col=美女&tag=诱惑&sort=0&pn=10&rn=10&p=channel&from=1");
+        View v=inflater.inflate(R.layout.fragment_photo_gallery,container,false);
+        mGridView= (GridView) v.findViewById(R.id.gridview);
+//        View v = inflater.inflate(R.layout.temp, container, false);
+//        mTextView = (TextView) v.findViewById(R.id.tv);
+//        mImageView= (ImageView) v.findViewById(R.id.imageview);
+        new BaiduConnectAsyncTask().execute("http://image.baidu.com/data/imgs?col=美女&tag=诱惑&sort=0&pn=20&rn=25&p=channel&from=1");
         return v;
     }
     private class BaiduConnectAsyncTask extends AsyncTask<String, Void, String> {
@@ -100,21 +104,31 @@ public class Fragment_PhotoGallery extends Fragment {
                 Image item=mImages.get(i);
 
                 String str=item.getId()+":"+item.toString()+","+"\n";
+                Log.d("baidu",str);
                 sb.append(str);
             }
 
-            mTextView.setText(sb.toString());
-            new GetBitmapAsynctask().execute(mImages.get(4).getDownloadUrl());
+            //mTextView.setText(sb.toString());
+            ArrayList<String> listThumbnailUrl=new ArrayList<>();
+            for (int i = 0; i < mImages.size(); i++) {
+                listThumbnailUrl.add(mImages.get(i).getThumbnailUrl());
+            }
+            new GetBitmapAsynctask().execute(listThumbnailUrl);
         }
     }
-    private class GetBitmapAsynctask extends AsyncTask<String,Void,Bitmap>{
+    private class GetBitmapAsynctask extends AsyncTask<ArrayList<String>,Void,ArrayList<Bitmap>>{
 
         @Override
-        protected Bitmap doInBackground(String... params) {
-
+        protected ArrayList<Bitmap>  doInBackground(ArrayList<String>... params) {
+            ArrayList<Bitmap> bitmaps=new ArrayList<>();
             try {
-                byte[] bitmap=new FlickrFetchr().getUrlBytes(params[0]);
-                return BitmapFactory.decodeByteArray(bitmap,0,bitmap.length);
+                for (int i = 0; i < params[0].size(); i++) {
+                    byte[] bitmap=new FlickrFetchr().getUrlBytes(params[0].get(i));
+                    //return BitmapFactory.decodeByteArray(bitmap,0,bitmap.length);
+                    bitmaps.add(BitmapFactory.decodeByteArray(bitmap,0,bitmap.length));
+                }
+                return bitmaps;
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -123,12 +137,49 @@ public class Fragment_PhotoGallery extends Fragment {
             return null;
         }
 
+
+
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            if(bitmap!=null){
-                mImageView.setImageBitmap(bitmap);
+        protected void onPostExecute(ArrayList<Bitmap> bitmaps) {
+
+
+            if(bitmaps!=null){
+                ArrayList<String> list=new ArrayList<>();
+                //ArrayList<Bitmap> bitmaps=new ArrayList<>();
+                for (int i = 0; i < mImages.size(); i++) {
+                    String str=mImages.get(i).getDesc();
+                    list.add(str);
+                    //bitmaps.add(bitmap);
+                }
+                //mImageView.setImageBitmap(bitmap);
+                mGridView.setAdapter(new GalleryAdapter(bitmaps));
+                mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getActivity(),mImages.get(position).toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //mGridView.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_gallery_item,list));
             }
         }
+    }
+    class GalleryAdapter extends ArrayAdapter<Bitmap>{
+
+
+        public GalleryAdapter(ArrayList<Bitmap> list) {
+            super(getActivity(),0, list);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                convertView=getActivity().getLayoutInflater().inflate(R.layout.gallery_item,parent,false);
+            }
+            ImageView view= (ImageView) convertView.findViewById(R.id.gallery_item_imageview);
+            view.setImageBitmap(getItem(position));
+            return convertView;
+        }
+
     }
 
 }
