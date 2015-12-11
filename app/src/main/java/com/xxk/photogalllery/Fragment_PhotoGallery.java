08eeeb74c690ff9a00bd8b22e4f83b2aed285066
jianxuewei <jianxuewei@126.com>
2015-12-11 22:43:09 +0800

    test

--- app/src/main/java/com/xxk/photogalllery/Fragment_PhotoGallery.java
+++ app/src/main/java/com/xxk/photogalllery/Fragment_PhotoGallery.java
@@ -48,7 +48,6 @@ public class Fragment_PhotoGallery extends Fragment {
         View v=inflater.inflate(R.layout.fragment_photo_gallery,container,false);
         mGridView= (GridView) v.findViewById(R.id.gridview);
 //        View v = inflater.inflate(R.layout.temp, container, false);
-//        mTextView = (TextView) v.findViewById(R.id.tv);
 //        mImageView= (ImageView) v.findViewById(R.id.imageview);
         new BaiduConnectAsyncTask().execute("http://image.baidu.com/data/imgs?col=美女&tag=诱惑&sort=0&pn=20&rn=25&p=channel&from=1");
         return v;
@@ -73,7 +72,7 @@ public class Fragment_PhotoGallery extends Fragment {
         @Override
         protected void onPostExecute(String result) {
             ArrayList<Image> items=new ArrayList<>();
-            JSONArray imageJsonArray = null;
+            JSONArray imageJsonArray;
             StringBuilder sb=new StringBuilder();
             try {
                 imageJsonArray = new JSONObject(result).getJSONArray("imgs");
